f850fb5682e482c16aa79a86661f866fa2ae6de4
Belleza <aishat.omojole@gmail.com>
2022-11-02 17:20:21 +0100

    Project initialised

---
+++ resources/views/zuriLaravel.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <div style='height:97vh;background-color: green;color:white;display:flex;justify-content:center;align-items:center;'>
+
+            <h1>Hello world!!!</h1>
+        </div>
+    </body>
+</html>
